5f103a7895ad5f2fa16128f081be78ac47193daf
Bhavesh083 <76737905+Bhavesh083@users.noreply.github.com>
2021-03-24 09:21:33 +0530

    Update imageslider.js

--- imageslider.js
+++ imageslider.js
@@ -3,7 +3,6 @@ const carouselImages = document.querySelectorAll('.carousel-slide img'
 
 const prevBtn = document.querySelector('#prevBtn');
 const nextBtn = document.querySelector('#nextBtn');
-
 let counter = 1;
 const size = carouselImages[0].clientWidth;
 
