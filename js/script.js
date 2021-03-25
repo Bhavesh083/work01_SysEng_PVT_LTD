f3bbee0ca96a86bbe1ea3bd065604460afa16ac9
Bhavesh083 <76737905+Bhavesh083@users.noreply.github.com>
2021-03-25 09:35:59 +0530

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,6 +1,10 @@
+// Drop down menu button functioning script in the resposnive mode
 var date = new Date();
 var latestYear = date.getFullYear();
 document.getElementById("date-year").innerHTML = latestYear;
+
+
+// Image Slider 
 
 function showNavigation(){
     var responsiveNavbarItems = document.getElementById('responsive-nav');
